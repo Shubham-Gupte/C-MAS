58047c638a52207f1d4dba1a1385cd5fa03c661a
Shubham-Gupte <shubhamgupte01@gmail.com>
2020-01-21 23:00:34 -0500

    Initialized Repo

---
+++ C-MAS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  C-MAS
+//
+//  Created by Shubham Gupte on 1/21/20.
+//  Copyright © 2020 Shubham Gupte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
